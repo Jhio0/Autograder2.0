f28cd1f584a653a8523844babd8457ebf5ae23ae
Jhio0 <sorianojhio218@gmail.com>
2025-03-03 13:25:46 -0700

    CheckUpProduction

--- AppBE/src/main/java/com/example/demo/user/UserRepository.java
+++ AppBE/src/main/java/com/example/demo/user/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.user;
 
-import com.example.demo.user.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
